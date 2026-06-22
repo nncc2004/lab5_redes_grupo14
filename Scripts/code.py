392034ce46b032ec4095a400b6ef88f13acb2678
nncc2004 <111405063+nncc2004@users.noreply.github.com>
2026-06-21 22:03:02 -0400

    Update code.py - fixing indentation

--- Scripts/code.py
+++ Scripts/code.py
@@ -6,136 +6,136 @@ from mn_wifi.net import Mininet_wifi
 
 
 def topology():
-    net = Mininet_wifi()
-	
+	net = Mininet_wifi()
+
 	# Configuracion de la perdida de senal 3.5+ 4/10 = 3.9
 	net.setPropagationModel(model="logDistance", exp=3.9)
-	
+
 	# Creacion de las AP's
 	'''
 	Las posiciones como se muetran en el diagrama
 	Los canales los puse asi porque intenta manener al menos 
 	4 canales de distancia con el AP mas cercano para evitar
 	interferencias
-	 REvisar README para el detalle
+	REvisar README para el detalle
 	'''
 	if True:
 		# ~ info("*** Creando los AP's\n")
 		ap1 = net.addAccessPoint(
-			'ap1',
-			ssid='ap1-ssid',
-			channel='1',
-			position='0,57,0'
+		'ap1',
+		ssid='ap1-ssid',
+		channel='1',
+		position='0,57,0'
 		)
 
 		ap2 = net.addAccessPoint(
-			'ap2',
-			ssid='ap2-ssid',
-			channel='6',
-			position='44.55,35.53,0'
+		'ap2',
+		ssid='ap2-ssid',
+		channel='6',
+		position='44.55,35.53,0'
 		)
 
 		ap3 = net.addAccessPoint(
-			'ap3',
-			ssid='ap3-ssid',
-			channel='11',
-			position='55.57,-12.68,0'
+		'ap3',
+		ssid='ap3-ssid',
+		channel='11',
+		position='55.57,-12.68,0'
 		)
 
 		ap4 = net.addAccessPoint(
-			'ap4',
-			ssid='ap4-ssid',
-			channel='1',
-			position='24.73,-51.36,0'
+		'ap4',
+		ssid='ap4-ssid',
+		channel='1',
+		position='24.73,-51.36,0'
 		)
 
 		ap5 = net.addAccessPoint(
-			'ap5',
-			ssid='ap5-ssid',
-			channel='11',
-			position='-24.73,-51.36,0'
+		'ap5',
+		ssid='ap5-ssid',
+		channel='11',
+		position='-24.73,-51.36,0'
 		)
 
 		ap6 = net.addAccessPoint(
-			'ap6',
-			ssid='ap6-ssid',
-			channel='1',
-			position='-55.57,-12.68,0'
+		'ap6',
+		ssid='ap6-ssid',
+		channel='1',
+		position='-55.57,-12.68,0'
 		)
 
 		ap7 = net.addAccessPoint(
-			'ap7',
-			ssid='ap7-ssid',
-			channel='6',
-			position='-44.55,35.53,0'
+		'ap7',
+		ssid='ap7-ssid',
+		channel='6',
+		position='-44.55,35.53,0'
 		)
 
 		ap8 = net.addAccessPoint(
-			'ap8',
-			ssid='ap8-ssid',
-			channel='6',
-			position='0,114,0'
+		'ap8',
+		ssid='ap8-ssid',
+		channel='6',
+		position='0,114,0'
 		)
 
 		ap9 = net.addAccessPoint(
-			'ap9',
-			ssid='ap9-ssid',
-			channel='10',
-			position='89.10,71.06,0'
+		'ap9',
+		ssid='ap9-ssid',
+		channel='10',
+		position='89.10,71.06,0'
 		)
 
 		ap10 = net.addAccessPoint(
-			'ap10',
-			ssid='ap10-ssid',
-			channel='4',
-			position='111.14,-25.36,0'
+		'ap10',
+		ssid='ap10-ssid',
+		channel='4',
+		position='111.14,-25.36,0'
 		)
 
 		ap11 = net.addAccessPoint(
-			'ap11',
-			ssid='ap11-ssid',
-			channel='6',
-			position='49.46,-102.72,0'
+		'ap11',
+		ssid='ap11-ssid',
+		channel='6',
+		position='49.46,-102.72,0'
 		)
 
 		ap12 = net.addAccessPoint(
-			'ap12',
-			ssid='ap12-ssid',
-			channel='3',
-			position='-49.46,-102.72,0'
+		'ap12',
+		ssid='ap12-ssid',
+		channel='3',
+		position='-49.46,-102.72,0'
 		)
 
 		ap13 = net.addAccessPoint(
-			'ap13',
-			ssid='ap13-ssid',
-			channel='9',
-			position='-111.14,-25.36,0'
+		'ap13',
+		ssid='ap13-ssid',
+		channel='9',
+		position='-111.14,-25.36,0'
 		)
 
 		ap14 = net.addAccessPoint(
-			'ap14',
-			ssid='ap14-ssid',
-			channel='11',
-			position='-89.10,71.06,0'
+		'ap14',
+		ssid='ap14-ssid',
+		channel='11',
+		position='-89.10,71.06,0'
 		)
-	
+
 	#Prueba intermedia, borrar despues
 	if True:
 		info("*** Inicializando los nodos\n")
 		c1 = net.addController('c1')
 		net.configureNodes()
 		net.plotGraph(
-			min_x=-150,
-			min_y=-150,
-			max_x=150,
-			max_y=150
+		min_x=-150,
+		min_y=-150,
+		max_x=150,
+		max_y=150
 		)
 
 		net.build()
 		c1.start()
 
 		for ap in [ap1, ap2, ap3, ap4, ap5, ap6, ap7,
-				   ap8, ap9, ap10, ap11, ap12, ap13, ap14]:
+		ap8, ap9, ap10, ap11, ap12, ap13, ap14]:
 			ap.start([c1])
 
 		CLI(net)
